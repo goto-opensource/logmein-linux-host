71a7a1395ea35625038a38082bb406dd149402c3
Janos Kasza <janos.kasza@logmein.com>
2018-09-11 18:27:54 +0200

    Added new icon and changed the description

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ import os
 from setuptools import setup
 
 setup(
-    name = "hello-janoskk",
+    name = "logmein-host",
     version = "1.0",
-    author = "Janos Kasza",
-    author_email = "janos.kasza@logmein.com",
-    description = ("Hello, janoskk"),
+    author = "LogMeIn, Inc.",
+    author_email = "logmein-linux@logmein.com",
+    description = ("The LogMeIn Host Software (Beta) for Linux"),
     license='License :: OSI Approved :: MIT License',
     keywords = "",
-    url = "http://logmein.com",
+    url = "http://repository.services.logmein.com/linux/",
     packages=['logmein_host', 'pytty'],
     package_data={'pytty': ['templates/*', 'static/*']},
     include_package_data=True,
-    long_description=("Hello, janoskk"),
+    long_description=(""),
     entry_points={
         'console_scripts': [
             'pytty = pytty.pytty:main_func',
             'logmein_host = logmein_host.snap_runner:main'
         ]
     }
-) 
+)
